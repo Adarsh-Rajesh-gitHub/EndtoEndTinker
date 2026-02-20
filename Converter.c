3eeac7a2785c3071553b7155ce1ebb93272c7a85
Adarsh Rajesh <ar77947@cs.utexas.edu>
2026-02-20 04:15:39 -0600

    corrected input validation

--- Converter.c
+++ Converter.c
@@ -271,14 +271,14 @@ int main(int argc, char *argv[]) {
     if(fread(&dataStart, sizeof(uint64_t), 1, fp) != 1) FAIL;
     if(fread(&dataSize, sizeof(uint64_t), 1, fp) != 1) FAIL;
     if (badMem(codeStart,(int)codeSize)) FAIL;
-    if (badMem(dataStart,(int)dataSize)) FAIL;
     if(codeSize % 4 != 0) FAIL;
     if(dataSize % 8 != 0) FAIL;
+    if(dataStart % 8 != 0) FAIL;
     if(codeSize > dataStart - codeStart) FAIL;
-    if(dataSize > 512*1024-codeStart) FAIL;
+    if(dataSize > 512*1024-dataStart) FAIL;
     pc = codeStart;
-    if(fread(&memory[pc], codeSize, 1, fp) != codeSize) FAIL;
-    if(fread(&memory[dataStart],dataSize, 1, fp) != dataSize) FAIL;
+    if(fread(&memory[pc], codeSize, 1, fp) != 1) FAIL;
+    if(fread(&memory[dataStart],dataSize, 1, fp) != 1) FAIL;
     registers[31] = sizeof(memory);
     fclose(fp);
 
